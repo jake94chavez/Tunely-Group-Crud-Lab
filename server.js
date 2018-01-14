740f2e7aef5f04ec5986b24338cb1347c06def79
Jake Chavez <jake94chavez@gmail.com>
2018-01-14 15:36:44 -0600

    adds bodyParser middleware

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 /************
  * DATABASE *
